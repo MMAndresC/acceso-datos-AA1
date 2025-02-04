c115b5a5872a6e7d37f8eed5c678994f466ec719
Mar <mandrescasabon@hotmail.com>
2025-02-04 13:55:02 +0100

    :construction: add object with regions

---
+++ src/main/java/com/svalero/AA1Tournament/constants/Constants.java
@@ -0,0 +1,14 @@
+package com.svalero.AA1Tournament.constants;
+
+import java.util.Map;
+
+public class Constants {
+
+    public static final Map<Integer, String> REGIONS = Map.of(
+            1, "North America",
+            2, "South America",
+            3, "Europe",
+            4, "Asia",
+            5, "Oceania"
+    );
+}
