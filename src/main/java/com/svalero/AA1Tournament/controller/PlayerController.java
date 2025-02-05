ef71c4008b06111e90c9173f505833e74e062cc4
Mar <mandrescasabon@hotmail.com>
2025-02-05 22:40:34 +0100

    :sparkles: player CRUD

---
+++ src/main/java/com/svalero/AA1Tournament/controller/PlayerController.java
@@ -0,0 +1,106 @@
+package com.svalero.AA1Tournament.controller;
+
+import com.svalero.AA1Tournament.domain.Player;
+import com.svalero.AA1Tournament.domain.dto.ErrorResponse;
+import com.svalero.AA1Tournament.domain.dto.player.PlayerInDto;
+import com.svalero.AA1Tournament.domain.dto.player.PlayerModifyDto;
+import com.svalero.AA1Tournament.exception.PlayerNotFoundException;
+import com.svalero.AA1Tournament.exception.TeamNotFoundException;
+import com.svalero.AA1Tournament.service.PlayerService;
+import jakarta.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/v1")
+public class PlayerController {
+
+    private final Logger logger = LoggerFactory.getLogger(PlayerController.class);
+    @Autowired
+    private PlayerService playerService;
+
+    @GetMapping("/players")
+    public ResponseEntity<List<Player>> getAll(){
+        this.logger.info("Listing all players...");
+        List<Player> allPlayers = this.playerService.getAll();
+        this.logger.info("End listing all players");
+        return new ResponseEntity<>(allPlayers, HttpStatus.OK);
+    }
+
+    @GetMapping("/players/{id}")
+    public ResponseEntity<Player> getById(@PathVariable long id) throws PlayerNotFoundException {
+        this.logger.info("Searching player by Id...");
+        Player player = this.playerService.getById(id);
+        this.logger.info("Player found");
+        return new ResponseEntity<>(player, HttpStatus.OK);
+    }
+
+    @PostMapping("/teams/{idTeam}/players")
+    public ResponseEntity<Player> add(@PathVariable long idTeam, @Valid @RequestBody PlayerInDto playerInDto) throws TeamNotFoundException {
+        this.logger.info("Adding a new player...");
+        Player player = this.playerService.add(playerInDto, idTeam);
+        this.logger.info("Player added");
+        return new ResponseEntity<>(player, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/players/{id}")
+    public ResponseEntity<Void> delete(@PathVariable long id) throws PlayerNotFoundException {
+        this.logger.info("Deleting a player by id...");
+        this.playerService.delete(id);
+        this.logger.info("Player deleted");
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("/players/{id}")
+    public ResponseEntity<Player> modify(@PathVariable long id, @Valid @RequestBody PlayerModifyDto playerModifyDto) throws PlayerNotFoundException, TeamNotFoundException {
+        this.logger.info("Modifying a player...");
+        Player modifiedPlayer = this.playerService.modify(id, playerModifyDto);
+        this.logger.info("Player modified");
+        return new ResponseEntity<>(modifiedPlayer, HttpStatus.OK);
+    }
+
+    //Excepciones
+    @ExceptionHandler(PlayerNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handlePlayerNotFoundException(PlayerNotFoundException exception) {
+        ErrorResponse error = ErrorResponse.generalError(404, exception.getMessage());
+        this.logger.error(exception.getMessage(), exception);
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(TeamNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleTeamNotFoundException(TeamNotFoundException exception) {
+        ErrorResponse error = ErrorResponse.generalError(404, exception.getMessage());
+        this.logger.error(exception.getMessage(), exception);
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> MethodArgumentNotValidException(MethodArgumentNotValidException exception) {
+        Map<String, String> errors = new HashMap<>();
+        exception.getBindingResult().getAllErrors().forEach(error -> {
+            String fieldName = ((FieldError) error).getField();
+            String message = error.getDefaultMessage();
+            errors.put(fieldName, message);
+        });
+        this.logger.error(exception.getMessage(), exception);
+        return new ResponseEntity<>(ErrorResponse.validationError(errors), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleException(Exception exception) {
+        ErrorResponse error = ErrorResponse.generalError(500, "Internal Server Error");
+        this.logger.error(exception.getMessage(), exception);
+        return new ResponseEntity<>(error, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+}
---
+++ src/main/java/com/svalero/AA1Tournament/service/PlayerService.java
@@ -0,0 +1,64 @@
+package com.svalero.AA1Tournament.service;
+
+import com.svalero.AA1Tournament.domain.Player;
+import com.svalero.AA1Tournament.domain.Team;
+import com.svalero.AA1Tournament.domain.dto.player.PlayerInDto;
+import com.svalero.AA1Tournament.domain.dto.player.PlayerModifyDto;
+import com.svalero.AA1Tournament.exception.PlayerNotFoundException;
+import com.svalero.AA1Tournament.exception.TeamNotFoundException;
+import com.svalero.AA1Tournament.repository.PlayerRepository;
+import com.svalero.AA1Tournament.repository.TeamRepository;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PlayerService {
+    @Autowired
+    private PlayerRepository playerRepository;
+
+    @Autowired
+    private TeamRepository teamRepository;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    public List<Player> getAll(){
+        return this.playerRepository.findAll();
+    }
+
+    public Player getById(long id) throws PlayerNotFoundException {
+        return this.playerRepository.findById(id).orElseThrow(PlayerNotFoundException::new);
+    }
+
+    public Player add(PlayerInDto newPlayer, long idTeam) throws TeamNotFoundException {
+        Team team = this.teamRepository.findById(idTeam).orElseThrow(TeamNotFoundException::new);
+        Player player = modelMapper.map(newPlayer, Player.class);
+        player.setTeam(team);
+        return this.playerRepository.save(player);
+    }
+
+    public void delete(long id) throws PlayerNotFoundException {
+        Player player = this.playerRepository.findById(id).orElseThrow(PlayerNotFoundException::new);
+        this.playerRepository.delete(player);
+    }
+
+    public Player modify(long id, PlayerModifyDto playerModifyDto) throws PlayerNotFoundException, TeamNotFoundException {
+        Player player = this.playerRepository.findById(id).orElseThrow(PlayerNotFoundException::new);
+        //if player changes team
+        if(player.getTeam().getId() != playerModifyDto.getIdTeam()){
+            long idTeam = playerModifyDto.getIdTeam();
+            Team team = this.teamRepository.findById(idTeam).orElseThrow(TeamNotFoundException::new);
+            player.setTeam(team);
+        }
+        //prevents it from trying to set the idTeam as the player id
+        //for some reason it was mapping the idTeam as player id
+        modelMapper.typeMap(PlayerModifyDto.class, Player.class)
+                .addMappings(mapper -> mapper.skip(Player::setId));
+        modelMapper.map(playerModifyDto, player);
+        this.playerRepository.save(player);
+        return player;
+    }
+}
